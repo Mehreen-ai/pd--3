279450ecfca2b2a897181479664ab744109e147f
Mehreen kalsoom <mehreenkalsoom8@gmail.com>
2024-09-24 22:42:25 +0500

    Add files via upload

---
+++ donkey.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<" ----------------------------------------- "<<endl;
+cout<<"            o      ^__^       "<<endl;
+cout<<"             o     (oo)\\________"<<endl;
+cout<<"                   (__)\\        )\\/\\"<<endl;
+cout<<"                        ||----w  |"<<endl;
+cout<<"                        ||      ||"<<endl;     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ t2.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+ main()
+{
+cout<<"                                                         SQUARE                 "<<endl;
+cout<<"                                                *************************       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *                       *       "<<endl;
+cout<<"                                                *************************       "<<endl;
+
+cout<<endl;
+cout<<endl;
+cout<<"                                                        TRIANGLE               "<<endl;
+cout<<endl;
+cout<<endl;
+cout<<"                                                            %                  "<<endl;
+cout<<"                                                           % %                 "<<endl;
+cout<<"                                                          %   %                "<<endl;
+cout<<"                                                         %     %               "<<endl;
+cout<<"                                                        %       %              "<<endl;
+cout<<"                                                       %         %             "<<endl;
+cout<<"                                                      %           %            "<<endl;
+cout<<"                                                     %             %           "<<endl;
+cout<<"                                                    %%%%%%%%%%%%%%%%%          "<<endl;
+cout<<endl;
+cout<<endl;
+
+
+cout<<"                                         CIRCLE                 "<<endl;
+cout<<endl;
+cout<<endl;
+cout<<"                                   ::::::::::::::::             "<<endl;
+cout<<"                                :::                :::          "<<endl;
+cout<<"                              ::                      ::        "<<endl;
+cout<<"                             ::                        ::       "<<endl;
+cout<<"                            ::                          ::      "<<endl;
+cout<<"                            :                            :      "<<endl;
+cout<<"                            :                            :      "<<endl;
+cout<<"                            :                            :      "<<endl;
+cout<<"                            ::                          ::      "<<endl;
+cout<<"                             ::                        ::       "<<endl;
+cout<<"                              ::                      ::        "<<endl;
+cout<<"                                :::                :::          "<<endl;
+cout<<"                                   ::::::::::::::::             "<<endl;
+cout<<endl;
+cout<<endl;
+
+
+
+cout<<"                                        PARALLELLOGRAM                       "<<endl;
+cout<<endl;
+cout<<endl;
+cout<<"                                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        "<<endl;
+cout<<"                                 @                                 @         "<<endl;
+cout<<"                                @                                 @          "<<endl;
+cout<<"                               @                                 @           "<<endl;
+cout<<"                              @                                 @            "<<endl;
+cout<<"                             @                                 @             "<<endl;
+cout<<"                            @                                 @              "<<endl;
+cout<<"                           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               "<<endl;
+cout<<endl;
+cout<<endl;
+
+cout<<"                                              HEXAGON                           "<<endl;
+cout<<endl;
+cout<<endl;
+
+cout<<"                                            ##########                          "<<endl;
+cout<<"                                         #              #                       "<<endl;
+cout<<"                                     #                     #                    "<<endl; 
+cout<<"                                  #                           #                 "<<endl; 
+cout<<"                               #                                 #              "<<endl; 
+cout<<"                                  #                           #                 "<<endl;                                       
+cout<<"                                     #                     #                    "<<endl;   
+cout<<"                                         #              #                       "<<endl;
+cout<<"                                            ##########                          "<<endl;
+}
